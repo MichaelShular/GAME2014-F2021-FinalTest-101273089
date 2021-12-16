c6766f0903757d72211a36272bd4e4d6cac2b09b
Mick <mickx.shular@gmail.com>
2021-12-16 12:36:59 -0500

    moved tiles in platform, added shrinking

--- Assets/[Scripts]/AddedScripts/ShrinkingPlatform.cs
+++ Assets/[Scripts]/AddedScripts/ShrinkingPlatform.cs
@@ -4,6 +4,7 @@
 //Summary: Controls aspects of shrinking platforms.
 //Revision History
 //-Added bobbing cycle
+//-Added Shrinking
 
 
 using System.Collections;
@@ -12,26 +13,73 @@ using UnityEngine;
 
 public class ShrinkingPlatform : MonoBehaviour
 {
-
+    [Header("Bobbing Movement")]
     [SerializeField] private float bobbingDistance;
     private Vector3 bobbingPosition;
+    [Header("Shrinking")]
+    private bool isShrinking;
+    [SerializeField] private float shrinkingSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         bobbingPosition = transform.position;
+        isShrinking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         _bobbingCycle();
-
+        _shinrking();
     }
-
     private void _bobbingCycle()
     {
         transform.position = new Vector3(bobbingPosition.x,
                 bobbingPosition.y + Mathf.PingPong(Time.time, bobbingDistance), 0.0f);
+    }
+
+    private void _shinrking()
+    {
+        if (isShrinking)
+        {
+            if (transform.localScale.x >= 0.01)
+            {
+                transform.localScale -= Vector3.one * shrinkingSpeed * Time.deltaTime;
+            }
+            else
+            {
+                StartCoroutine(turnBackOnCollision());
+            }
+        }
+        else
+        {
+            if (transform.localScale.x <= 1)
+            {
+                GetComponent<BoxCollider2D>().enabled = true;
+                transform.localScale += Vector3.one * shrinkingSpeed * Time.deltaTime;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isShrinking = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isShrinking = false;
+        }
+    }
+
+    IEnumerator turnBackOnCollision()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        yield return new WaitForSeconds(1.0f);
+        GetComponent<BoxCollider2D>().enabled = true;
     }
 }
